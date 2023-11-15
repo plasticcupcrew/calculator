6c98a97d65eceb2b352ecd297449b1be62709328
Michael Borrello <mborrello1@gmail.com>
2023-11-15 11:06:10 -0500

    Creat operate function and variables
    Create function that takes in two numbers and an operator and
    calls one of the math functions based on the operator.

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
     return a / b;
 }
+
+let num1;
+let num2
+let op;
+
+const operate = function(num1, op, num2) {
+    switch (op) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+    }
+}
